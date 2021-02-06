ae4a24d8173dbaa1a93bc19908a8ef709aa9000e
Andrey Sereda <andreysereda@ya.ru>
2021-02-06 22:58:21 +0300

    добавил компоненты App, главной страницы и карточки места с валидацией передаваемых данных

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
+const Setting = {
+  PLACES_FOUND: 312,
+  LOCATIONS: [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`]
+};
+
+ReactDOM.render(
+    <App
+      placesFound={Setting.PLACES_FOUND}
+      locations={Setting.LOCATIONS}
+    />,
+    document.querySelector(`#root`)
+);
