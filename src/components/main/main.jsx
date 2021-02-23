dab7ece881b53d66abbb68ad550b935050d5121c
Andrey Sereda <andreysereda@ya.ru>
2021-02-23 20:40:35 +0300

    фильтрация предложений по городу в main

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -6,6 +6,7 @@ import OffersList from '../offers-list/offers-list';
 
 const Main = (props) => {
   const {placesFound, locations, offers} = props;
+  const activeCity = `Amsterdam`;
 
   return (
     <main className="page__main page__main--index">
@@ -15,7 +16,7 @@ const Main = (props) => {
           <ul className="locations__list tabs__list">
             {
               locations.map((item, i) => <li key={item + i} className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
+                <a className={item === activeCity ? `locations__item-link tabs__item tabs__item--active` : `locations__item-link tabs__item`} href="#">
                   <span>{item}</span>
                 </a>
               </li>)
@@ -43,7 +44,7 @@ const Main = (props) => {
                 <li className="places__option" tabIndex="0">Top rated first</li>
               </ul>
             </form>
-            <OffersList offers={offers}/>
+            <OffersList offers={offers.filter((offer) => offer.city.name === activeCity)}/>
           </section>
           <div className="cities__right-section">
             <section className="cities__map map"></section>
