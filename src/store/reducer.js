06dde3936e60556ce4a86b6459037911468c2418
Andrey Sereda <andreysereda@ya.ru>
2021-03-15 22:42:22 +0300

    добавил reducer, actionType

---
+++ src/store/reducer.js
@@ -0,0 +1,20 @@
+import offers from './mocks/offers';
+import ActionType from './action';
+
+const initialState = {
+  currentCity: `Paris`,
+  offers
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.CHANGE_CITY:
+      return {
+        ...state,
+        currentCity: action.payload
+      };
+  }
+  return state;
+};
+
+export {reducer};
