d65150058928d0c19c1c03578d329011a81d30e6
Andrey Sereda <andreysereda@ya.ru>
2021-03-18 16:54:00 +0300

    подключил redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import offers from './mocks/offers';
 import {reducer} from './store/reducer';
@@ -10,7 +11,7 @@ const Setting = {
   LOCATIONS: [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`]
 };
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
     <Provider store={store}>
