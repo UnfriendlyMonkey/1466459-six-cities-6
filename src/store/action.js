f503a15997638947cb22c8ab9a4ce4958c5d4da2
Andrey Sereda <andreysereda@ya.ru>
2021-03-30 22:23:10 +0300

    использовал самописные функции на createAction из rtk

--- src/store/action.js
+++ src/store/action.js
@@ -1,3 +1,5 @@
+import {createAction} from '@reduxjs/toolkit';
+
 export const ActionType = {
   CHANGE_CITY: `changeCity`,
   SET_ACTIVE_OFFER: `setActiveOffer`,
@@ -12,59 +14,67 @@ export const ActionType = {
   UPDATE_PROPERTY: `updateProperty`,
 };
 
-export const setActiveCity = (city) => ({
-  type: ActionType.CHANGE_CITY,
-  payload: city,
+export const setActiveCity = createAction(ActionType.CHANGE_CITY, (city) => {
+  return {
+    payload: city,
+  };
 });
 
-export const setActiveOffer = (offer) => ({
-  type: ActionType.SET_ACTIVE_OFFER,
-  payload: offer,
+export const setActiveOffer = createAction(ActionType.SET_ACTIVE_OFFER, (offer) => {
+  return {
+    payload: offer,
+  };
 });
 
-export const loadOffers = (offers) => ({
-  type: ActionType.LOAD_OFFERS,
-  payload: offers
+export const loadOffers = createAction(ActionType.LOAD_OFFERS, (offers) => {
+  return {
+    payload: offers
+  };
 });
 
-export const loadProperty = (property) => ({
-  type: ActionType.LOAD_PROPERTY,
-  payload: property
+export const loadProperty = createAction(ActionType.LOAD_PROPERTY, (property) => {
+  return {
+    payload: property
+  };
 });
 
-export const loadComments = (comments) => ({
-  type: ActionType.LOAD_COMMENTS,
-  payload: comments
+export const loadComments = createAction(ActionType.LOAD_COMMENTS, (comments) => {
+  return {
+    payload: comments
+  };
 });
 
-export const loadNearPlaces = (offers) => ({
-  type: ActionType.LOAD_NEAR_PLACES,
-  payload: offers
+export const loadNearPlaces = createAction(ActionType.LOAD_NEAR_PLACES, (offers) => {
+  return {
+    payload: offers
+  };
 });
 
-export const loadFavorite = (offers) => ({
-  type: ActionType.LOAD_FAVORITE,
-  payload: offers
+export const loadFavorite = createAction(ActionType.LOAD_FAVORITE, (offers) => {
+  return {
+    payload: offers
+  };
 });
 
-export const updateProperty = (property) => ({
-  type: ActionType.UPDATE_PROPERTY,
-  payload: property
+export const updateProperty = createAction(ActionType.UPDATE_PROPERTY, (property) => {
+  return {
+    payload: property
+  };
 });
 
-export const requireAuthorization = (status, login = ``) => ({
-  type: ActionType.REQUIRED_AUTHORIZATION,
-  payload: {
-    status,
-    login
-  }
+export const requireAuthorization = createAction(ActionType.REQUIRED_AUTHORIZATION, (status, login = ``) => {
+  return {
+    payload: {
+      status,
+      login
+    },
+  };
 });
 
-export const reset = () => ({
-  type: ActionType.RESET,
-});
+export const reset = createAction(ActionType.RESET);
 
-export const redirectToRoute = (url) => ({
-  type: ActionType.REDIRECT_TO_ROUTE,
-  payload: url,
+export const redirectToRoute = createAction(ActionType.REDIRECT_TO_ROUTE, (url) => {
+  return {
+    payload: url,
+  };
 });
