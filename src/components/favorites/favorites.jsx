26a26d84dc61066e56a89c357a9d0d048f46cb81
Andrey Sereda <andreysereda@ya.ru>
2021-04-01 18:02:50 +0300

    поправил разметку в favorites

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -24,33 +24,35 @@ const Favorites = (props) => {
   }
 
   const emptyList = (
-    <section className="favorites favorites--empty">
-      <h1 className="visually-hidden">Favorites (empty)</h1>
-      <div className="favorites__status-wrapper">
-        <b className="favorites__status">Nothing yet saved.</b>
-        <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
-      </div>
-    </section>
-  );
-  const cities = [...new Set(offers.map((offer) => offer.city.name))];
-  const noEmptyList = (
-    <section className="favorites">
-      <h1 className="favorites__title">Saved listing</h1>
-      <ul className="favorites__list">
-        {cities.map((city) => (
-          <FavoritesCity key={cities.indexOf(city)} city={city} offers={offers.filter((offer) => offer.city.name === city)}/>
-        ))}
-      </ul>
-    </section>
-  );
-
-  return (
-    <main className="page__main page__main--favorites">
+    <main className="page__main page__main--favorites page__main--favorites-empty">
       <div className="page__favorites-container container">
-        {offers.length === 0 ? emptyList : noEmptyList}
+        <section className="favorites favorites--empty">
+          <h1 className="visually-hidden">Favorites (empty)</h1>
+          <div className="favorites__status-wrapper">
+            <b className="favorites__status">Nothing yet saved.</b>
+            <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+          </div>
+        </section>
       </div>
     </main>
   );
+  const cities = [...new Set(offers.map((offer) => offer.city.name))];
+  const noEmptyList = (
+    <main className="page__main page__main--favorites">
+      <div className="page__favorites-container container">
+        <section className="favorites">
+          <h1 className="favorites__title">Saved listing</h1>
+          <ul className="favorites__list">
+            {cities.map((city) => (
+              <FavoritesCity key={cities.indexOf(city)} city={city} offers={offers.filter((offer) => offer.city.name === city)}/>
+            ))}
+          </ul>
+        </section>
+      </div>
+    </main>
+  );
+
+  return offers.length === 0 ? emptyList : noEmptyList;
 };
 
 Favorites.propTypes = {
