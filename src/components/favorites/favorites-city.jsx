e99f33a31cedbfa98ffe6f267aadadc82ada9e50
Andrey Sereda <andreysereda@ya.ru>
2021-04-01 15:34:09 +0300

    Сделал названия городов в favorites ссылками на main с предложениями в соответствующем городе

--- src/components/favorites/favorites-city.jsx
+++ src/components/favorites/favorites-city.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {offerType} from '../../types/offer';
 import PlaceCard from '../place-card/place-card';
+import {setActiveCity} from '../../store/action';
+import {connect} from 'react-redux';
+import {AppRoute} from '../../const';
+import browserHistory from '../../browser-history';
 
 const FavoritesCity = (props) => {
-  const {city, offers} = props;
+  const {city, offers, changeActiveCity} = props;
+
+  const handleCityClick = (evt) => {
+    evt.preventDefault();
+    changeActiveCity(city);
+    browserHistory.push(AppRoute.MAIN);
+  };
 
   return (
     <li className="favorites__locations-items">
       <div className="favorites__locations locations locations--current">
         <div className="locations__item">
-          <a className="locations__item-link" href="#">
+          <Link
+            className="locations__item-link"
+            to={AppRoute.MAIN}
+            onClick={handleCityClick}>
             <span>{city}</span>
-          </a>
+          </Link>
         </div>
       </div>
       <div className="favorites__places">
@@ -28,7 +42,15 @@ FavoritesCity.propTypes = {
   city: PropTypes.string,
   offers: PropTypes.arrayOf(
       offerType
-  ).isRequired
+  ).isRequired,
+  changeActiveCity: PropTypes.func.isRequired,
 };
 
-export default FavoritesCity;
+const mapDispatchToProps = (dispatch) => ({
+  changeActiveCity(city) {
+    dispatch(setActiveCity(city));
+  },
+});
+
+export {FavoritesCity};
+export default connect(null, mapDispatchToProps)(FavoritesCity);
